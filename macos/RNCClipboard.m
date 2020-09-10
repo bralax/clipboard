b3a78c5d2401694e1a65f4bf4d2479b806c9e63d
Brandon Lax <brlax@optonline.net>
2020-09-09 22:12:31 -0400

    feat macos support

--- macos/RNCClipboard.m
+++ macos/RNCClipboard.m
@@ -4,29 +4,8 @@
 #import <AppKit/AppKit.h>
 #import <React/RCTBridge.h>
 #import <React/RCTEventDispatcher.h>
+#import <React/RCTConvert.h>
 
-
-@implementation RCTConvert (PasteBoardType)
-  RCT_ENUM_CONVERTER(NSPasteboardType,
-                     (@{ @"URL" : @(NSPasteboardTypeString),
-                        @"color" : @(NSPasteboardTypeColor),
-                        @"NSFileContentsPboardType" : @(NSFileContentsPboardType),
-                        @"NSFindPanelSearchOptionsPboardType" : @(NSFindPanelSearchOptionsPboardType),
-                        @"NSPasteboardTypeHTML" : @(NSPasteboardTypeHTML),
-                        @"NSPasteboardTypeMultipleTextSelection" : @(NSPasteboardTypeMultipleTextSelection),
-                        @"NSPasteboardTypePDF" : @(NSPasteboardTypePDF),
-                        @"NSPasteboardTypePNG" : @(NSPasteboardTypePNG),
-                        @"NSPasteboardTypeRTF" : @(NSPasteboardTypeRTF),
-                        @"NSPasteboardTypeRTFD" : @(NSPasteboardTypeRTFD),
-                        @"NSPasteboardTypeRuler" : @(NSPasteboardTypeRuler),
-                        @"NSPasteboardTypeSound" : @(NSPasteboardTypeSound),
-                        @"NSPasteboardTypeString" : @(NSPasteboardTypeString),
-                        @"NSPasteboardTypeTabularText" : @(NSPasteboardTypeTabularText),
-                        @"NSPasteboardTypeTextFinderOptions" : @(NSPasteboardTypeTextFinderOptions),
-                        @"NSPasteboardTypeTIFF" : @(NSPasteboardTypeTIFF),
-                      }),
-                      NSPasteboardTypeString, integerValue)
-@end
 
 @implementation RNCClipboard
 
@@ -38,20 +17,71 @@ RCT_EXPORT_MODULE();
 }
 
 
-
 RCT_EXPORT_METHOD(setString:(NSString *)content)
 {
+  NSLog(@"%@", content);
   NSPasteboard *clipboard = [NSPasteboard generalPasteboard];
-  [clipboard setString:content forType:NSPasteboardTypeString]
-  //clipboard.string = (content ? : @"");
+  [clipboard prepareForNewContentsWithOptions:0];
+  NSLog(@"%d",[clipboard setString:content forType:NSPasteboardTypeString]);
 }
+
 
 RCT_EXPORT_METHOD(getString:(RCTPromiseResolveBlock)resolve
                   reject:(__unused RCTPromiseRejectBlock)reject)
 {
   NSPasteboard *clipboard = [NSPasteboard generalPasteboard];
-  
-  //resolve((clipboard.string ? : @""));
+  NSMutableString *output = [[NSMutableString alloc] init];
+  NSString *add = [clipboard stringForType:NSPasteboardTypeString];
+  if (add != nil) {
+    [output appendString:add];
+  }
+  resolve([NSString stringWithString:output]);
 }
+
+/*
+ The below code would allow the ability to set the type of the contents on the
+ clipboard. This code is not currently in use as the code is currently set
+ up to match the already existing interface.
+ RCT_EXPORT_METHOD(setStringWithType:(NSString *)content:(NSString *)type)
+{
+    
+  NSPasteboard *clipboard = [NSPasteboard generalPasteboard];
+  NSPasteboardType pType = [self PasteBoardType:type];
+  NSLog(@"%@ %@ %@", content, type, pType);
+    [clipboard declareTypes:[NSArray arrayWithObject:pType] owner:nil];
+  NSLog(@"%d",[clipboard setString:content forType:pType]);
+  //clipboard.string = (content ? : @"");
+}
+
+RCT_EXPORT_METHOD(getStringWithType:(NSString *)type:(RCTPromiseResolveBlock)resolve
+                  reject:(__unused RCTPromiseRejectBlock)reject)
+{
+  NSPasteboard *clipboard = [NSPasteboard generalPasteboard];
+  NSPasteboardType pType = [self PasteBoardType:type];
+  resolve(( [clipboard stringForType:pType]? : @""));
+}
+
+-(NSPasteboardType)PasteBoardType:(NSString *)type
+{
+    NSDictionary* dict = @{ @"url" : NSPasteboardTypeString,
+       @"color" : NSPasteboardTypeColor,
+       @"file" : NSFileContentsPboardType,
+       @"findPanel" : NSFindPanelSearchOptionsPboardType,
+       @"html" : NSPasteboardTypeHTML,
+       @"multipleText" : NSPasteboardTypeMultipleTextSelection,
+       @"pdf" : NSPasteboardTypePDF,
+       @"png" : NSPasteboardTypePNG,
+       @"rtf" : NSPasteboardTypeRTF,
+       @"rtfd" : NSPasteboardTypeRTFD,
+       @"ruler" : NSPasteboardTypeRuler,
+       @"sound" : NSPasteboardTypeSound,
+       @"string" : NSPasteboardTypeString,
+       @"tabularText" : NSPasteboardTypeTabularText,
+       @"finder" : NSPasteboardTypeTextFinderOptions,
+       @"tiff" : NSPasteboardTypeTIFF,
+    };
+    return dict[type];
+}*/
+
 
 @end
